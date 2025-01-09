628beac5b7b981855a94d5bdfce802c1cf644fdf
Kat <kat.m.deans@gmail.com>
2025-01-09 12:46:12 -0500

    Fontawesome icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css'
 import Nav from './components/Nav';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars)
+
 
 function App() {
   return (
     <div className="App">
-      <Nav>
+      <Nav/>
     </div>
   );
 }
