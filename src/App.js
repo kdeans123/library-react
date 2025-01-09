e8e794851288999e9cc82e6ee4c93763bdee31fe
Kat <kat.m.deans@gmail.com>
2025-01-09 13:32:41 -0500

    Adding logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import './index.css'
 import Nav from './components/Nav';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LibraryLogo from '../assets/Library.svg';
+
+// we need to import library logo like, this, we do not add them like we did in html directly in the code giving directory 
 
 const Nav = () => {
     return (
         <nav>
             <div className="nav__container">
                 <a href="/">
-                <img src="" alt="" className="logo" />
+                <img src={LibraryLogo} alt="" className="logo" />
                 </a>
                 <ul className='nav__links'>
                     <li className='nav__list'>
@@ -23,3 +26,5 @@ const Nav = () => {
         </nav>
     )
 }
+
+export default Nav;
