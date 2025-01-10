521c33c544e5277fecfc17be6c297a660080cde7
Kat <kat.m.deans@gmail.com>
2025-01-10 10:07:50 -0500

    Updating styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Landing from "./components/Landing";
-import Highlights from "./components/Highlights";
-import Featured from "./components/Featured";
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
+import Landing from "../components/Landing";
+import Highlights from "../components/Highlights";
+import Featured from "../components/Featured";
+import Discounted from "../components/Discounted";
+import Explore from "../components/Explore";
 
 const Home = () => {
   return (
