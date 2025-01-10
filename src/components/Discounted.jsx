fe259e3fbc4afb64eca0baf8b0056d88bc82eb7a
Kat <kat.m.deans@gmail.com>
2025-01-09 20:28:24 -0500

    Showing first 8 discouned books

--- src/components/Discounted.jsx
+++ src/components/Discounted.jsx
@@ -7,11 +7,14 @@ const Discounted = () => {
         <section id="recent">
             <div className="container">
                 <div className="row">
-                    <div className="section__title">
+                    <h2 className="section__title">
                         Discounted <span className="purple">Books</span>
-                    </div>
+                    </h2>
                     <div className="books">
-                        {books.map(book => <Book book={book} key={book.id} /> )}
+                        {books
+                        .filter(book => book.salePrice > 0 )
+                        .slice(0, 8)
+                        .map(book => <Book book={book} key={book.id} /> )}
 
                     </div>
 
@@ -26,3 +29,7 @@ export default Discounted;
 
 
 // we want to show books that are on the sale, so their sale price is not equal null 
+// to show books that are on sale we will use filter property, it checks and if it is true then it returns every element that is true. 
+// so we want to show the book if the sale price > 0. but we only want first 8 books so we do .slide(0, 8)
+// in js we can add these function like that onto each other 
+// and then map over each element of this array and turn each element into this html tag 
