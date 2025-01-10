65c276cbd25d3e074a4755015aaddd81c776400d
Kat <kat.m.deans@gmail.com>
2025-01-09 23:00:07 -0500

    Books routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './index.css';
 import Nav from "./components/Nav";
-import Landing from "./components/Landing";
-import Highlights from "./components/Highlights";
-import Featured from "./components/Featured";
-import Discounted from "./components/Discounted";
-import Explore from './components/Explore';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 
 function App() {
   return (
-    <div className="App">
-      <Nav/>
-      <Landing/>
-      <Highlights/>
-      <Featured/>
-      <Discounted/>
-      <Explore/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Home />
+        <Footer />
+      </div>
+    </Router>
+
   );
 }
 
 export default App;
+
+// first import BrowserRouter and rename it as Router, then we need to wrap this app in a router
+//  then we cut out what is not repeating (so we leave Nav, and Footer), and then we create new src folder and name it pages 
+// and inside that folder we create Home.jsx and paste all the components there: Landing, highlights, featured, discounted, explore and wrap it in a fragment
