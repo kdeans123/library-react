e840e3786ca105a8fe1d705d4a740ae29398e3a3
Kat <kat.m.deans@gmail.com>
2025-01-10 10:39:30 -0500

    Update

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Highlight from "../components/ui/Highlight";
 
-const Highlight = ({ icon, title, para }) => {
+const Highlights = () => {
   return (
-                <div className="highlight">
-                    <div className="highlight__img">
-                         {icon}
-                    </div>
-                         <h3 className="highlight__subtitle">{title}</h3>
-                         <p className="highlight__para">{para}</p>
+    <section id="Highlights">
+        <div className="container">
+            <div className="row">
+                <h2 className="section__title">Why choose <span className="purple">Library</span>
+                </h2>
+                    <div className='highlight__wrapper'>
+                        <Highlight 
+                            icon={ <FontAwesomeIcon icon="bolt" />} 
+                            title="Easy and quick"
+                            para="Get access to the book you purchased instantly."/>
+                        <Highlight 
+                            icon={ <FontAwesomeIcon icon="book-open" />} 
+                            title="10,000+ Books"
+                            para="Library has books in all your favourite categories."/>   
+                        <Highlight 
+                            icon={ <FontAwesomeIcon icon="tags" />} 
+                            title="Affordable"
+                            para="Get your hands on popular books for as little as $10"/>
+                    </div>           
+            </div>
+        </div>
+    </section>
+  )
+}
 
-                </div>
-  );
-};
-
-
-export default Highlight;
+export default Highlights;
--- src/components/ui/highlight.jsx
+++ src/components/ui/highlight.jsx
@@ -5,9 +5,10 @@ const Highlight = ({ icon, title, para }) => {
                 <div className="highlight">
                     <div className="highlight__img">
                          {icon}
+                    </div>
                          <h3 className="highlight__subtitle">{title}</h3>
                          <p className="highlight__para">{para}</p>
-                    </div>
+
                 </div>
   );
 };
